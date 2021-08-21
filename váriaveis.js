a663419f677c6990b9076f690bb26885185d23ea
Isabeli Reik <isabelireik2@gmail.com>
2021-08-21 18:16:55 +0000

    variáveis

---
+++ váriaveis.js
@@ -0,0 +1,11 @@
+let name = "Isabeli";
+const age = 26; //esse tipo de variável não pode ser alterada
+
+console.log(name);
+console.log(age);
+
+name = "Isabeli Reik";
+console.log(name);
+
+name = null;
+console.log(name);
