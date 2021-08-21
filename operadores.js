39a4817e75eb7daa86d44a3affbb52e07e79dffc
Isabeli Reik <isabelireik2@gmail.com>
2021-08-21 18:32:18 +0000

    operadores

---
+++ operadores.js
@@ -0,0 +1,23 @@
+//operadores matemáticos
+
+let resultado = 5 + 4;
+resultado = 5 - 4;
+resultado = 5 * 4;
+resultado = 5 / 4;
+console.log(resultado);
+
+//pegar o resto de uma divisão
+let resto = 5 % 4;
+console.log(resto);
+
+//primeiro ele faz divisões e multiplicações, depois soma e subtração
+resultado = 5 + 4 * 3 - 8;
+console.log(resultado);
+
+resultado = resultado + 5;
+console.log(resultado);
+
+resultado += 5;
+resultado /= 5;
+
+resultado++;
