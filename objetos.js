3d63911c39b2e62f7c183dd267046b32a8f787dc
Isabeli Reik <isabelireik2@gmail.com>
2021-08-23 23:35:23 +0000

    objetos

---
+++ objetos.js
@@ -0,0 +1,12 @@
+// objetos - conjunto de variáveis
+
+const pessoa = {
+  nome: "Isabeli",
+  idade: 26,
+  ehMaiorDeIdade: true,
+  altura: 1.69
+};
+
+console.log(pessoa);
+
+console.log(pessoa.altura);
