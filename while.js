438eab193de1ea92bfed5b02d28b8f736afda637
Isabeli Reik <isabelireik2@gmail.com>
2021-08-28 23:32:08 +0000

    while

---
+++ while.js
@@ -0,0 +1,10 @@
+//loops
+
+const listaDeCompras = ["Arroz", "Batata", "Maça"];
+
+let indice = 0;
+while(indice < listaDeCompras.length){
+  console.log(listaDeCompras[indice]);
+  indice++;
+  //aqui no js ele imprimi a ultima soma feita no while na tela
+}
