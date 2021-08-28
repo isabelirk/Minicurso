3fe7858bd1fcc2e2e671127c30ce104d3a0a0b02
Isabeli Reik <isabelireik2@gmail.com>
2021-08-28 00:31:24 +0000

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//arrays ou vetores
+
+const listaDeCompras = ["Arroz", "Batata", "Maça"];
+//indice 0, 1, 2
+
+console.log(listaDeCompras[0]);
+console.log(listaDeCompras[1]);
+console.log(listaDeCompras[2]);
